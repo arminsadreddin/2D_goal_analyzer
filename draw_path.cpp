74cbb882240269a91a1a85a925adc80dac32d75d
arminsadreddin <armin.sadreddin@gmail.com>
2018-12-27 18:11:13 +0330

    ready to draw paths

---
+++ draw_path.cpp
@@ -0,0 +1,43 @@
+#include "draw_path.h"
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+
+draw_path::draw_path()
+{
+
+}
+void draw_path::draw_goal_paths(MainWindow * w , vector<vector<pair<double, double> > > all_paths){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //    for(int a = 0 ; a < all_paths.size() ; a++){
+
+    //        for(int p = 0 ; p < all_paths.at(a).size() ; p++){
+
+
+
+    //        }
+
+    //    }
+
+
+}
---
+++ draw_path.h
@@ -0,0 +1,16 @@
+#ifndef DRAW_PATH_H
+#define DRAW_PATH_H
+#include <iostream>
+#include <vector>
+#include <mainwindow.h>
+#include <qpainter.h>
+using namespace std;
+
+class draw_path
+{
+public:
+    draw_path();
+    void draw_goal_paths(MainWindow * w , vector<vector<pair<double , double> > > all_paths);
+};
+
+#endif // DRAW_PATH_H
